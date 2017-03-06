6058232104ccf36864942439fe50d9c8dbfe41ae
IURII CH <hydrol.777@gmail.com>
2017-03-07 02:15:46 +0700

    -v2

--- app/src/main/java/com/iuriich/KrgadosPlayLauncher/App.java
+++ app/src/main/java/com/iuriich/KrgadosPlayLauncher/App.java
@@ -1,18 +1,27 @@
 package com.iuriich.KrgadosPlayLauncher;
 
+import android.app.ActivityManager;
 import android.app.Application;
+import android.content.ComponentName;
 import android.content.Context;
+import android.content.Intent;
 import android.view.WindowManager;
 
+import com.iuriich.KrgadosPlayLauncher.activities.LauncherActivity;
 import com.iuriich.KrgadosPlayLauncher.utils.StatusbarBlockerViewGroup;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class App extends Application {
 
     private static App instance;
 
     private ArrayList<StatusbarBlockerViewGroup> blockersList;
+
+    private Timer timer;
 
     public static App getInstance() {
         return instance;
@@ -23,6 +32,24 @@ public class App extends Application {
         super.onCreate();
         instance = this;
         blockersList = new ArrayList<>();
+
+        if(timer == null) {
+            timer = new Timer();
+            timer.scheduleAtFixedRate(new TimerTask() {
+                synchronized public void run() {
+                    ActivityManager am = (ActivityManager) getSystemService(ACTIVITY_SERVICE);
+                    List<ActivityManager.RunningTaskInfo> taskInfo = am.getRunningTasks(1);
+
+                    ComponentName componentInfo = taskInfo.get(0).topActivity;
+                    if (!blockersList.isEmpty() && !componentInfo.getPackageName().equals(Preferences.getInstance().getPref(Preferences.Pref.appname)) &&
+                            !componentInfo.getPackageName().equals("com.iuriich.KrgadosPlayLauncher")) {
+                        Intent intent = new Intent(App.this, LauncherActivity.class);
+                        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                        startActivity(intent);
+                    }
+                }
+            }, 0, 3000);
+        }
     }
 
     public void removeBlockers() {
